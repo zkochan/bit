de2b5678dfff6ef72cc5f5d736187cfc479f13f3
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-08-02 19:48:41 +0200

    add comments in workspace
    add different scope for empty and one for demo components so all future components are created under demo.

--- scopes/react/react/templates/react-workspace/files/workspace-config.ts
+++ scopes/react/react/templates/react-workspace/files/workspace-config.ts
@@ -4,7 +4,9 @@ import { getWorkspaceConfigTemplateParsed, stringifyWorkspaceConfig } from '@tea
 export async function workspaceConfig({ name, defaultScope, empty }: WorkspaceContext) {
   const configParsed = await getWorkspaceConfigTemplateParsed();
   configParsed['teambit.workspace/workspace'].name = name;
-  configParsed['teambit.workspace/workspace'].defaultScope = defaultScope || 'company.scope';
+  configParsed['teambit.workspace/workspace'].defaultScope = empty
+    ? defaultScope || 'company.scope'
+    : defaultScope || 'company.demo';
   configParsed['teambit.dependencies/dependency-resolver'].packageManager = 'teambit.dependencies/pnpm';
   configParsed['teambit.dependencies/dependency-resolver'].policy = {
     dependencies: {},
@@ -18,15 +20,24 @@ export async function workspaceConfig({ name, defaultScope, empty }: WorkspaceCo
   configParsed['teambit.workspace/variants'] = empty
     ? {
         '*': {
+          // all components use the default react env by teambit
           'teambit.react/react': {},
         },
       }
     : {
-        '{templates/ui/**}, {templates/pages/**}': {
-          'company.scope/templates/envs/my-react': {},
+        '{**/ui/**}, {**/pages/**}': {
+          // all the components using the ui and pages namespace will use the customized react environment in the workspace
+          'company.demo/templates/envs/my-react': {},
+          // to use the default react env by teambit instead of the custom env uncomment the following line and remove the line above:
+          // "teambit.harmony/react": {},
         },
-        '{templates/envs/**}': {
+        '{**/envs/**}, {**/extensions/**}': {
+          // all the components using the envs and extensions namespace will use the aspect environment in the workspace
           'teambit.harmony/aspect': {},
+        },
+        '{**/content/**}': {
+          // all the components using the content namespace will use the mdx environment in the workspace
+          'teambit.mdx/mdx': {},
         },
       };
 
