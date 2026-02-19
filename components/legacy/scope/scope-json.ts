95d4f20a22244e57824e8f68548bbd85d2ac0480
Zoltan Kochan <z@kochan.io>
2026-02-19 22:25:03 +0100

    Revert "fix: remove private modifier from ScopeJson.getTrackLane to fix capsule build"
    This reverts commit f739685c97ee9c9096c20be9bbf285464718ec3d.

--- components/legacy/scope/scope-json.ts
+++ components/legacy/scope/scope-json.ts
@@ -134,7 +134,7 @@ export class ScopeJson {
   }
 
   trackLane(trackLaneData: TrackLane) {
-    const existing = this._getTrackLane(trackLaneData.localLane);
+    const existing = this.getTrackLane(trackLaneData.localLane);
     if (existing) {
       existing.remoteLane = trackLaneData.remoteLane;
       existing.remoteScope = trackLaneData.remoteScope;
@@ -150,7 +150,7 @@ export class ScopeJson {
     this.lanes.tracking.splice(index, 1);
     this.hasChanged = true;
   }
-  _getTrackLane(localLane: string): TrackLane | undefined {
+  private getTrackLane(localLane: string): TrackLane | undefined {
     return this.lanes.tracking.find((t) => t.localLane === localLane);
   }
   setLaneAsNew(laneName: string) {
